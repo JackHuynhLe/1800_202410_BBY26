a800761a020d43671b005ce25b2bb2a412f5e1a9
YuanXiQWQ <xjr815226031@outlook.com>
2024-03-05 18:53:28 -0800

    Style and preparation for js
    login.html:
    1.add navbar and footer
    2.link to login.css
    
    login.css:
    1.initilise

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,21 @@
+//---------------------------------
+// Your own functions here
+//---------------------------------
+
+
 function sayHello() {
-    
+  //do something
 }
-//sayHello();
+//sayHello();    //invoke function
+
+//------------------------------------------------
+// Call this function when the "logout" button is clicked
+//-------------------------------------------------
+function logout() {
+  firebase.auth().signOut().then(() => {
+    // Sign-out successful.
+    console.log("logging out user");
+  }).catch((error) => {
+    // An error happened.
+  });
+}
