08c6dfec4a550bde341f0f3b3e2601b37f4f3767
YuanXiQWQ <xjr815226031@outlook.com>
2024-03-21 00:41:19 -0700

    fix bugs

--- public/scripts/firebaseAPI.js
+++ public/scripts/firebaseAPI.js
@@ -1,19 +1,21 @@
-//----------------------------------------
-//  Your web app's Firebase configuration
-//----------------------------------------
+// Import the functions
+import {initializeApp} from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
+import {getFirestore} from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js";
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyAPr5p921A7Nvox_17pcrADPIcXgONlaGo",
     authDomain: "bby26-c858c.firebaseapp.com",
+    databaseURL: "https://bby26-c858c-default-rtdb.firebaseio.com",
     projectId: "bby26-c858c",
     storageBucket: "bby26-c858c.appspot.com",
     messagingSenderId: "569835543614",
-    appId: "1:569835543614:web:fc118410cebf4f1169856f"
+    appId: "1:569835543614:web:fc118410cebf4f1169856f",
+    measurementId: "G-3QTZ7MGJ7W"
 };
 
-//--------------------------------------------
-// initialize the Firebase app
-// initialize Firestore database if using it
-//--------------------------------------------
-const app = firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-const storage = firebase.storage();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+// Export the database to firebase.mjs
+export {db}
