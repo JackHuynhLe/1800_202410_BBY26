69b432847640e7e214ca45f315b9e69b3ac5de1b
Uppnoor <uppnoor41@gmail.com>
2024-03-21 13:22:53 -0700

    Revised authentication file. it is not connect to our direct firebase and will need much work to connect both to the current project.

---
+++ Project/scripts/login.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    var modals = document.querySelectorAll('.modal');
+    M.Modal.init(modals);
+
+    var items = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(items);
+
+});
+
+const guideList = document.querySelector('.guides');
+
+//setup guide
+export const setupGuides = (data) => {
+
+if (data.length) {
+
+let html = '';
+data.forEach(doc => {
+    const guide = doc.data();
+    const li = `
+    <li>
+        <div class='collapsible-header grey lighten-4'>${guide.title}</div>
+        <div class='collapsible-body white'>${guide.content}</div>
+    </li>
+    `;
+    html += li;
+})
+
+guideList.innerHTML = html;
+} else {
+    guideList.innerHTML = '<h5 class="center-align">Login to view content</h5>';
+}
+
+}
+
+const loggedOutLinks = document.querySelectorAll('.logged-out');
+const loggedInLinks = document.querySelectorAll('.logged-in');
+
+export const setupUI = (user) => {
+    if (user) {
+        //toggle UI elements
+        loggedInLinks.forEach(item => item.style.display = 'block');
+        loggedOutLinks.forEach(item => item.style.display = 'none');
+    } else {
+        //toggle UI elements
+        loggedInLinks.forEach(item => item.style.display = 'none');
+        loggedOutLinks.forEach(item => item.style.display = 'block');
+    }
+}
