8b91635e72e49fe900c3586a5da8042b5353896f
YuanXi <xjr815226031@outlook.com>
2024-03-27 17:41:58 -0700

    fix bugs

--- scripts/client.js
+++ scripts/client.js
@@ -1,36 +1,46 @@
 /*HTML
 * */
 
+/**
+ * To insert the bottom navbar and footer into each page,
+ * and load the language file for it if exists.
+ */
 document.addEventListener("DOMContentLoaded", () => {
-  // Get bottom_navbar.html
-  fetch("../text/bottom_navbar.html")
-    .then(response => response.text())
-    .then(html => {
-      document.getElementById("bottomNavContainer").innerHTML = html;
-    })
-    .catch(error => console.error("Error loading the bottom navbar:", error));
+    let bottomNavLoaded = fetch("../text/bottom_navbar.html")
+      .then(response => response.text())
+      .then(html => {
+          document.getElementById("bottomNavContainer").innerHTML = html;
+      });
 
-// Get footer.html
-  fetch("../text/footer.html")
-    .then(response => response.text())
-    .then(html => {
-      document.getElementById("footerContainer").innerHTML = html;
-    })
-    .catch(error => console.error("Error loading the footer:", error));
+    let footerLoaded = fetch("../text/footer.html")
+      .then(response => response.text())
+      .then(html => {
+          document.getElementById("footerContainer").innerHTML = html;
+      });
 
+    Promise.all([bottomNavLoaded, footerLoaded]).then(() => {
+        // Check if the preferred language is saved and change it if it is.
+        const preferredLanguage = localStorage.getItem("preferredLanguage");
+        if (preferredLanguage) {
+            changeLanguage(preferredLanguage);
+        }
+    }).catch(error => console.error("Error initializing the page:", error));
 });
+
 /*Footer
 * */
 
-// Language selection
+/**
+ * To display the language list when the user clicks on the language button.
+ */
 document.getElementById("footerContainer").addEventListener("click", () => {
-  const languageList = document.getElementById("language-list")
+    const languageList = document.getElementById("language-list")
 
-  if (languageList.style.display === "none") {
-    languageList.style.display = "flex";
-  } else {
-    languageList.style.display = "none";
-  }
+    if (languageList.style.display === "none") {
+        languageList.style.display = "flex";
+    } else {
+        languageList.style.display = "none";
+    }
 });
 
 /**
@@ -39,35 +49,35 @@ document.getElementById("footerContainer").addEventListener("click", () => {
 // Check if the preferred language is saved
 const preferredLanguage = localStorage.getItem("preferredLanguage");
 if (preferredLanguage) {
-  changeLanguage(preferredLanguage);
+    changeLanguage(preferredLanguage);
 }
 
 function changeLanguage(language) {
-  // Save the selected language
-  window.localStorage.setItem("preferredLanguage", language);
+    // Save the selected language
+    window.localStorage.setItem("preferredLanguage", language);
 
-  // Load the new language
-  fetch(`../languages/${language}.json`)
-    .then(response => response.json())
-    .then(data => {
-      let strings = data.Strings;
-      document.querySelectorAll(".lang-text").forEach(element => {
-        let key = element.getAttribute("data-key");
-        // Check if the key exists in the strings object
-        if (key in strings) {
-          // Set the text content of the element
-          element.textContent = strings[key];
-        }
-      });
-    })
-    .catch(error => console.error("Error loading the language file:", error));
+    // Load the new language
+    fetch(`../languages/${language}.json`)
+      .then(response => response.json())
+      .then(data => {
+          let strings = data.Strings;
+          document.querySelectorAll(".lang-text").forEach(element => {
+              let key = element.getAttribute("data-key");
+              // Check if the key exists in the strings object
+              if (key in strings) {
+                  // Set the text content of the element
+                  element.textContent = strings[key];
+              }
+          });
+      })
+      .catch(error => console.error("Error loading the language file:", error));
 }
 
 document.body.addEventListener("click", function (event) {
-  // Check if the clicked element is an interlanguage link
-  if (event.target.matches(".interlanguage-link, .interlanguage-link *")) {
-    const element = event.target.closest(".interlanguage-link");
-    const selectedLanguageCode = element.getAttribute("data-code");
-    changeLanguage(selectedLanguageCode);
-  }
+    // Check if the clicked element is an interlanguage link
+    if (event.target.matches(".interlanguage-link, .interlanguage-link *")) {
+        const element = event.target.closest(".interlanguage-link");
+        const selectedLanguageCode = element.getAttribute("data-code");
+        changeLanguage(selectedLanguageCode);
+    }
 });
