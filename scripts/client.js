7ea5d812aa3401c7ebc7c3c4e854a6ebb9f3e98a
YuanXi <xjr815226031@outlook.com>
2024-04-04 11:16:15 -0700

    Rename index to main, login to index and add current location btn
    add script for currentLocationBtn
    
    comment, optimise and fix
    
    add comment,
    optimise client.js
    fix currentLocationBtn

--- scripts/client.js
+++ scripts/client.js
@@ -2,17 +2,6 @@
  * The main JavaScript file for the Stardust.
  * */
 
-// Function to retrieve localised strings with fallback
-function getLocalisedString(key) {
-    // Wait for the currentLanguageStrings to be loaded
-    if (window.currentLanguageStrings && key in window.currentLanguageStrings) {
-        return window.currentLanguageStrings[key];
-    } else {
-        // Fallback to English or a default message before the language files are loaded
-        return `Loading...`;
-    }
-}
-
 /**
  * Function about loading page
  * */
@@ -37,18 +26,41 @@ document.addEventListener("DOMContentLoaded", () => {
 
     Promise.all([bottomNavLoaded, footerLoaded]).then(() => {
         // Check if the preferred language is saved and change it if it is.
-        const preferredLanguage = localStorage.getItem("preferredLanguage") || 'en-UK'; // Default to 'en-UK' if not set
+        // Default to 'en-UK' if not set
+        const preferredLanguage = localStorage.getItem("preferredLanguage") || 'en-UK';
         changeLanguage(preferredLanguage);
     }).catch(error => console.error("Error initializing the page:", error));
 });
 
 /**
- * Change the language based on the selected language.
+ * Functions about change the language based on the selected language.
  * */
 {
-    // Global variable to store current language strings
+    /**
+     * Function to retrieve localised strings with fallback
+     * @param key - The key of the string
+     * @return {*|string} - The localised string
+     */
+    function getLocalisedString(key) {
+        // Wait for the currentLanguageStrings to be loaded
+        if (window.currentLanguageStrings && key in window.currentLanguageStrings) {
+            return window.currentLanguageStrings[key];
+        } else {
+            // Fallback to English or a default message before the language files are loaded
+            return `Loading...`;
+        }
+    }
+
+    /**
+     * Global variable to store current language strings
+     * @type {{}} - The current language strings
+     */
     window.currentLanguageStrings = {};
 
+    /**
+     * Function to change the language
+     * @param language - The language to change to
+     */
     function changeLanguage(language) {
         // Save the selected language
         window.localStorage.setItem("preferredLanguage", language);
@@ -59,16 +71,27 @@ document.addEventListener("DOMContentLoaded", () => {
             .then(data => {
                 // Store the loaded language strings
                 window.currentLanguageStrings = data.Strings;
+                // Update the language attribute of the HTML element
+                updateLangAttribute(language);
                 // Update page content with the loaded language
                 updatePageContent();
                 // To update all placeholders
                 updatePlaceholders();
-                // Update the language attribute of the HTML element
-                updateLangAttribute(language);
             })
             .catch(error => console.error("Error loading the language file:", error));
     }
 
+    /**
+     * Function to update the language attribute of the HTML element
+     * @param language - The language to set
+     */
+    function updateLangAttribute(language) {
+        document.documentElement.lang = language;
+    }
+
+    /**
+     * Function to update the page content with the loaded language
+     */
     function updatePageContent() {
         document.querySelectorAll(".lang-text").forEach(element => {
             const key = element.getAttribute("data-key");
@@ -78,6 +101,9 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 
+    /**
+     * Function to update all placeholders
+     */
     function updatePlaceholders() {
         document.querySelectorAll('input[data-key]').forEach(input => {
             const key = input.getAttribute('data-key');
@@ -87,26 +113,25 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 
-    function updateLangAttribute(language) {
-        document.documentElement.lang = language;
-    }
+    /**
+     * Function to handle the click event of the language list
+     */
+    document.addEventListener("DOMContentLoaded", () => {
+        document.getElementById("footerContainer").addEventListener("click", () => {
+            const languageList = document.getElementById("language-list");
+            languageList.style.display = languageList.style.display === "none" ? "flex" : "none";
+        });
+
+        document.body.addEventListener("click", function (event) {
+            if (event.target.matches(".interlanguage-link, .interlanguage-link *")) {
+                const element = event.target.closest(".interlanguage-link");
+                const selectedLanguageCode = element.getAttribute("data-code");
+                changeLanguage(selectedLanguageCode);
+            }
+        });
+    });
 }
 
-// Event listeners for language selection
-document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("footerContainer").addEventListener("click", () => {
-        const languageList = document.getElementById("language-list");
-        languageList.style.display = languageList.style.display === "none" ? "flex" : "none";
-    });
-
-    document.body.addEventListener("click", function (event) {
-        if (event.target.matches(".interlanguage-link, .interlanguage-link *")) {
-            const element = event.target.closest(".interlanguage-link");
-            const selectedLanguageCode = element.getAttribute("data-code");
-            changeLanguage(selectedLanguageCode);
-        }
-    });
-});
 /**
  * Interactive of the page.
  */
--- scripts/map.js
+++ scripts/map.js
@@ -1,58 +1,90 @@
-function initMap() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition, handleError);
-    } else {
+/**
+ * Initialise the map
+ * */
+{
+    /**
+     * Function to initialise the map
+     */
+    function initMap() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition, handleError);
+        } else {
+            alert(getLocalisedString("geoNotSupported"));
+        }
+    }
+
+    /*
+    * Handles errors during geolocation fetching
+    * */
+    function handleError(error) {
+        console.error('Geolocation error:', error);
         alert(getLocalisedString("geoNotSupported"));
     }
-}
 
-/*
-* Handles errors during geolocation fetching
-* */
-function handleError(error) {
-    console.error('Geolocation error:', error);
-    alert(getLocalisedString("geoNotSupported"));
-}
+    /*
+    * Generates URL for the iframe to display the map centered around the specified coordinates
+    * */
+    function generateMapURL(latitude, longitude, latOffset = 0.005, longOffset = 0.005) {
+        return `https://www.openstreetmap.org/export/embed.html?bbox=${longitude - longOffset},${latitude - latOffset},${longitude + longOffset},${latitude + latOffset}&layer=mapnik&marker=${latitude},${longitude}`;
+    }
 
-/*
-* Generates URL for the iframe to display the map centered around the specified coordinates
-* */
-function generateMapURL(latitude, longitude, latOffset = 0.005, longOffset = 0.005) {
-    return `https://www.openstreetmap.org/export/embed.html?bbox=${longitude - longOffset},${latitude - latOffset},${longitude + longOffset},${latitude + latOffset}&layer=mapnik&marker=${latitude},${longitude}`;
-}
+    /**
+     * Function to get the current location of the user
+     * @param position - The position of the user
+     * @param zoomLevel - The smaller the number, the more detailed the information displayed.
+     */
+    function showPosition(position, zoomLevel) {
+        const mapFrame = document.getElementById('mapFrame');
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        zoomLevel ??= 0.01;
+        mapFrame.src = generateMapURL(latitude, longitude, zoomLevel, zoomLevel);
+    }
 
-function showPosition(position) {
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
-    const zoomLevel = 13;
-    const mapFrame = document.getElementById('mapFrame');
-    mapFrame.src = generateMapURL(latitude, longitude, 0.01, 0.01);
-}
+    /**
+     * Function to search the given query on the map and update the iframe with the result.
+     */
+    function searchOnMap() {
+        const query = document.getElementById('site-search').value;
+        const url =
+            `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(query)}`;
 
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                if (data.length > 0) {
+                    let lat = parseFloat(data[0].lat);
+                    let lon = parseFloat(data[0].lon);
+                    // update the iframe
+                    document.getElementById('mapFrame').src = generateMapURL(lat, lon);
+                } else {
+                    alert(getLocalisedString("noResultsFound"));
+                }
+            })
+            .catch(error => {
+                console.error('Search error:', error);
+                alert(getLocalisedString("searchError"));
+            });
+    }
+
+    window.onload = initMap;
+}
 /**
- * Function to search the given query on the map and update the iframe with the result.
+ * Interactive functions of the map in the main.html
  */
-function searchOnMap() {
-    const query = document.getElementById('site-search').value;
-    const url =
-        `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(query)}`;
-
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            if (data.length > 0) {
-                let lat = parseFloat(data[0].lat);
-                let lon = parseFloat(data[0].lon);
-                // update the iframe
-                document.getElementById('mapFrame').src = generateMapURL(lat, lon);
-            } else {
-                alert(getLocalisedString("noResultsFound"));
-            }
-        })
-        .catch(error => {
-            console.error('Search error:', error);
-            alert(getLocalisedString("searchError"));
-        });
+{
+    /**
+     * Function to get the current location of the user
+     */
+    const currentLocationBtn = document.getElementById('currentLocationBtn');
+    currentLocationBtn.addEventListener('click', () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                const zoomLevel = 0.001;
+                showPosition(position, zoomLevel);
+            }, handleError);
+        } else {
+            alert(getLocalisedString("geoNotSupported"));
+        }
+    });
 }
-
-window.onload = initMap;
