6d418a9107e519a58618c2a08b22b0d913f2eab3
YuanXi <xjr815226031@outlook.com>
2024-04-04 15:33:16 -0700

    Basically finish community page

---
+++ scripts/community.js
@@ -0,0 +1,10 @@
+/**
+ * Functions about the interaction with the community page
+ */
+{
+/* TODO: Add interactive function including:
+* 1. When click on the post-userProfile, turn to the post user's profile page
+* 2. When click on the post-overviewContainer, turn to the post detail page
+* 3. When click on the post-info-postLocation, turn to the main page and locate the location
+* */
+}
